09ba1f69332758dcb23c301345e65958cab92d3d
Nathan Orlowski <nathan@Someone.local>
2024-12-11 17:30:30 +0200

    1

--- ScrabbleTest.java
+++ ScrabbleTest.java
@@ -2,6 +2,7 @@ import java.io.*;
 
 public class ScrabbleTest {
     public static void main(String[] args) {
+        testPlayGameWithMockInput();
         if (args.length == 0) {
             System.out.println("Please provide a specific test to run: isWordInDictionary, wordScore, createHand, playHand, playHandWithMockInput, or playGameWithMockInput.");
             return;
